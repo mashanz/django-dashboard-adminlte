b2d324653c1c9ac23d5901a0e7ccbe85a30422ff
Mashanz <mangatkk@gmail.com>
2020-09-03 11:09:04 +0700

    Custom Layouts

--- app/urls.py
+++ app/urls.py
@@ -9,8 +9,10 @@ from app import views
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
-
+    path('templates', views.index, name='home'),
+    path('api', views.ini_api),
+    path('date', views.ini_current_datetime),
+    path('', views.web_index),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
--- app/views.py
+++ app/views.py
@@ -6,12 +6,14 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django import template
+
 
 @login_required(login_url="/login/")
 def index(request):
     return render(request, "index.html")
+
 
 @login_required(login_url="/login/")
 def pages(request):
@@ -19,17 +21,45 @@ def pages(request):
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-        
+
         load_template = request.path.split('/')[-1]
-        html_template = loader.get_template( load_template )
+        html_template = loader.get_template(load_template)
         return HttpResponse(html_template.render(context, request))
-        
+
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template( 'page-404.html' )
+        html_template = loader.get_template('page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
-    
-        html_template = loader.get_template( 'page-500.html' )
+
+        html_template = loader.get_template('page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+def ini_api(request):
+    print("OH")
+    x = [1, 4, 2, 5, 7, 8, 6, 3, 8, 7, 9, 8, 0]
+    k = 0
+    for i in x:
+        k += i
+    response = JsonResponse(
+        {
+            'foo': 'bar',
+            'x': x,
+            'k': k
+        }
+    )
+    return response
+
+
+def ini_current_datetime(request):
+    # response = HttpResponse("Here's the text of the Web page.")
+    response = HttpResponse("Text only, please.", content_type="text/plain")
+    # response = HttpResponse(b'Bytestrings are also accepted.')
+    # response = HttpResponse(memoryview(b'Memoryview as well.'))
+    return response
+
+
+def web_index(request):
+    return render(request, "web/index.html", {})
