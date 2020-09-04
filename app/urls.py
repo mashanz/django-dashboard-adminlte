c3bc7b7ce7b2d37222aae8d253b4844a134d408d
Mashanz <mangatkk@gmail.com>
2020-09-04 16:10:16 +0700

    add Table Styles

--- app/urls.py
+++ app/urls.py
@@ -13,14 +13,19 @@ urlpatterns = [
     # The home page
     path('templates', views.index, name='home'),
     path('api', views.ini_api),
-    path('test_api', test_view.test_api),
     path('date', views.ini_current_datetime),
     path('', views.web_index),
     path('line_follower', views.web_line_follower),
     path('manipulator', views.web_manipulator),
     path('humanoid', views.web_humanoid),
     path('about_this_page', views.web_about_this_page),
+
+    # Unrivalry App
     path('unrivalry_add_person', main_view.unrivalry_add_person),
+
+    # Test
+    path('test_api', test_view.test_api),
+
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
--- unrivalry/forms.py
+++ unrivalry/forms.py
@@ -4,29 +4,30 @@ from .models import PersonModel
 
 class AddPersonForm(forms.ModelForm):
 
-    # SHIRT_SIZES = (
-    #     ('S', 'Small'),
-    #     ('M', 'Medium'),
-    #     ('L', 'Large'),
-    # )
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
 
-    # full_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             "placeholder": "Full Name",
-    #             "class": "form-control"
-    #         }
-    #     )
-    # )
+    full_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Full Name",
+                "class": "form-control"
+            }
+        )
+    )
 
-    # shirt_size = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             "placeholder": "Full Name",
-    #             "class": "form-control"
-    #         }
-    #     )
-    # )
+    shirt_size = forms.CharField(
+        widget=forms.Select(
+            attrs={
+                "name": "shirt_size",
+                "placeholder": "Shirt Size",
+                "class": "form-control"
+            }, choices=SHIRT_SIZES
+        )
+    )
 
     class Meta:
         model = PersonModel
